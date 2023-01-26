fbd0c25d57142e2ffdf3717474adffe2310903c9
vidal theo <26835772+Dalvii@users.noreply.github.com>
2023-01-26 13:41:56 +0100

    add front

---
+++ public/script.js
@@ -0,0 +1,199 @@
+let myModal = document.getElementById('exampleModal')
+let myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus()
+    console.log('test')
+})
+
+
+const BACK_URL = 'http://localhost:8080/api'
+
+const channelsListBox = document.querySelector('#channels')
+const messagesListBox = document.querySelector('#messages')
+const sendMessageBtn = document.querySelector('#sendMsgBtn');
+const createChannelBtn = document.querySelector('#createChannel');
+const createChannelInput = document.querySelector('#createChannelInput');
+
+
+let channelList = []
+let currentChannel = null;
+let messagesList = []
+
+
+// Initialiser l'application
+async function start() {
+    await fetchChannels()
+    if (!channelList[0]) return console.log('Aucun channel')
+    selectChannel(channelList[0].id)
+    // setInterval(() => {
+    //     fetchMessages(currentChannel.id)
+    // }, 3000);
+}
+start()
+
+
+// Récuperer la liste des Channels et les insérer dans le DOM
+async function fetchChannels() {
+    const channels = await getData('/channel');
+    channelList = channels
+    console.log(channels)
+    channelsListBox.innerHTML = ''
+
+    for (let i = 0; i < channels.length; i++) {
+        if (channels[i].type != 'channel') continue;
+        const item = document.createElement('li')
+        console.log(item)
+        item.className = 'channel bg-channel rounded-3 px-2 py-1 mb-2 text-white'
+        item.id = 'channel_' + channels[i].id
+        item.innerText = channels[i].name
+        channelsListBox.append(item)
+
+        item.addEventListener("click", function (e) {
+            console.log(e.target)
+            const id = e.target.id.substr(8)
+            selectChannel(id)
+        });
+    }
+
+    const addChannelButton = document.createElement('li')
+    channelsListBox.append(addChannelButton)
+    addChannelButton.outerHTML = `<li class="bg-channel rounded-3 px-2 py-1 mb-2 text-white" id="myInput" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            + Creer un channel
+        </li>`
+}
+
+
+
+
+// Fonction qui change l'URL, entoure le bouton du channel correspondant, et fetch les messages
+function selectChannel(id) {
+    console.log(id)
+    currentChannel = channelList.find(channel => channel.id == id)
+    const notSelectedChannel = document.querySelectorAll('.channel')
+    notSelectedChannel.forEach(channel => {
+        channel.classList.remove('selected')
+    });
+    const selectedChannel = document.querySelector('#channel_' + id)
+    selectedChannel.classList.add('selected')
+    window.history.pushState(currentChannel.id, currentChannel.id, currentChannel.id);
+
+    fetchMessages(id)
+}
+
+
+
+// Récuperer la liste des Messages d'un channel donné et les insérer dans le DOM
+async function fetchMessages(channelId) {
+    const messages = await getData('/channel/' + channelId);
+    messagesList = messages
+    console.log(messages)
+    messagesListBox.innerHTML = ''
+
+    if (messages.length < 1) {
+        const message = document.createElement('p')
+        message.innerText = "Aucun message"
+        message.style.color = 'white'
+        messagesListBox.append(message)
+    }
+    
+    for (let i = 0; i < messages.length; i++) {
+        const item = document.createElement('li')
+        item.className = 'bg-chat-item rounded-3 p-2 mb-2 text-white'
+        item.id = 'message_' + messages[i].id
+
+        const author = document.createElement('h6')
+        author.innerText = messages[i].sender
+
+        const message = document.createElement('p')
+        message.classList = 'text-wrap my-2'
+        message.innerText = messages[i].content
+        item.append(author)
+        item.append(message)
+
+        messagesListBox.append(item)
+    }
+}
+
+
+// Ecouter quand l'utilisateur envoie un message et envoie la requete
+sendMessageBtn.addEventListener("keydown", async function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        const message = event.target.value
+        
+        const sentMessage = await postData('/channel/' + currentChannel.id, {
+            content: message
+        })
+        console.log('send')
+        event.target.value = ''
+        fetchMessages(currentChannel.id)
+    }
+});
+
+
+// Ecouter quand l'utilisateur envoie un message et envoie la requete
+createChannelBtn.addEventListener("click", async function (event) {
+    const channelName = createChannelInput.value
+    createChannelInput.value = ''
+
+    const createdChannel = await postData('/channel', {
+        name: channelName,
+        type: 'channel'
+    })
+    console.log(createdChannel)
+    await fetchChannels()
+    selectChannel(createdChannel.id)
+    fetchMessages(currentChannel.id)
+});
+
+
+
+// Fonction pour envoyer une requete POST avec des données
+async function postData(url = '', data = {}) {
+    const response = await fetch(BACK_URL + url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': getCookie('token')
+        },
+        body: JSON.stringify(data)
+    });
+    return response.json();
+}
+
+// Fonction pour envoyer une requete GET
+async function getData(url = '') {
+    const response = await fetch(BACK_URL + url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': getCookie('token')
+        },
+    });
+    return response.json();
+}
+
+// Fonction pour stocker un cookie
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Fonction pour récupérer un cookie
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
